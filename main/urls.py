9f5418555f321ee706fb63f4e03b480c24ff4cd3
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-07-11 11:42:06 +0300

    movie_page introduced

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("index/", views.index, name="index"),
     path("", views.home, name="home"),
-    path("api/movies/", views.movies_json, name="movies_json")
+    path("api/movies/", views.movies_json, name="movies_json"),
+    path('<int:movie_id>',views.movie_page,name="movie_page")
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
-from main.models import Movie, Ratings
+from main.models import Movie, Ratings, Genre, Collection
 import pprint
 
 
@@ -13,6 +13,38 @@ def index(request):
 
 def home(request):
     return render(request, "main/home.html")
+
+
+def movie_page(response, movie_id):
+    movie = get_object_or_404(Movie, movie_id=movie_id)
+    if movie:
+        movie_ratings = get_object_or_404(Ratings, movie=movie)
+        movie_genres = movie.genres.all()
+        movie_collection = movie.belongs_to_collection
+
+        movie_data = {
+            'title': movie.title,
+            'overview': movie.overview,
+            'release_date': movie.release_date,
+            'runtime': movie.runtime,
+            'adult':movie.adult,
+            'poster_url': movie.poster_url,
+            'backdrop_url': movie.backdrop_url,
+            'tagline': movie.tagline,
+            'original_title': movie.original_title,
+            'tmdb_popularity': movie_ratings.tmdb_popularity,
+            'tmdb_vote_average': movie_ratings.tmdb_vote_average,
+            'imdb_rating': movie_ratings.imdb_rating,
+            'imdb_vote_count': movie_ratings.imdb_vote_count,
+            'genres': movie_genres
+        }
+        if movie_collection:
+            movie_data['collection_name'] = movie_collection['name'],
+            movie_data['collection_poster_path'] = movie_collection['poster_path'],
+            movie_data['collection_backdrop_path'] = movie_collection['backdrop_path']
+
+        pprint.pprint(movie_data)
+        return render(response, 'main/movie_page.html', movie_data)
 
 
 def movies_json(request):
